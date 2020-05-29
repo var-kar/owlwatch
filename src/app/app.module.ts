03fda833a9177420fd550cb38aed0ef5ba4f4d14
Ramz <Ramz@Ramyas-MacBook-Pro.local>
2020-05-30 02:08:06 +0530

    added controls, background and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { ControlsComponent } from './Component/controls/controls.component';
+import { SearchComponent } from './Component/search/search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HeaderComponent, ControlsComponent, SearchComponent ],
+	imports: [ BrowserModule ],
+	providers: [],
+	bootstrap: [ AppComponent ],
+	schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class AppModule { }
+export class AppModule {}
